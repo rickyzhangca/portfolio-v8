74689c27ab9126de3b58252f35cafcb5564e6eb4
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2025-03-08 11:28:56 -0500

    test redirects

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { ReactFlowProvider } from '@xyflow/react';
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
   createBrowserRouter,
@@ -9,18 +9,6 @@ import {
 import { TooltipProvider } from './components/Tooltip.tsx';
 import { Flow } from './Flow.tsx';
 import './index.css';
-
-const ExternalRedirect = ({ to }: { to: string }) => {
-  useEffect(() => {
-    window.location.href = to;
-  }, [to]);
-
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      Redirecting to {to}...
-    </div>
-  );
-};
 
 const router = createBrowserRouter([
   {
@@ -32,18 +20,7 @@ const router = createBrowserRouter([
     ),
   },
   {
-    path: '/spark2025checklist',
-    element: (
-      <ExternalRedirect to="https://www.figma.com/design/gV50xRFbRvzi6vOVUgXZxv/Checklist?node-id=1-2&t=mKW18MDE7Na8Vazj-4" />
-    ),
-  },
-  {
-    path: '/spark2025deck',
-    element: (
-      <ExternalRedirect to="https://www.figma.com/slides/pob8Ta54ZwugxGMgjON8Bz/ricky-spark2025?node-id=3-2&t=8PFb34Dx7DDaVun0-1" />
-    ),
-  },
-  {
+    // Catch-all route that redirects to the root path
     path: '*',
     element: <Navigate to="/" replace />,
   },
