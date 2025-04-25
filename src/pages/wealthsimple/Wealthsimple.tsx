9be7dfe66628b30b1f392214b8bcdd80e4a7253d
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2025-04-24 21:45:28 -0400

    update

--- src/pages/wealthsimple/Wealthsimple.tsx
+++ src/pages/wealthsimple/Wealthsimple.tsx
@@ -1,7 +1,8 @@
 import { HeaderCard } from '../../components/HeaderCard';
 import { HighlightCard } from '../../components/HighlightCard';
 import { PageLayout } from '../../components/PageLayout';
-import shh from './shh.webp';
+import darkMode from './dark-mode.webp';
+import snackbar from './snackbar.webp';
 
 export const Wealthsimple = () => {
   return (
@@ -10,11 +11,10 @@ export const Wealthsimple = () => {
         title="Wealthsimple, Design System"
         subtitle="Building something people love to use everyday."
       />
-      <HighlightCard
-        image={shh}
-        notShipped
-        title="Cooking something exciting this year...!"
-      />
+      <div className="grid gap-2 md:grid-cols-2">
+        <HighlightCard image={darkMode} title="Web dark mode" />
+        <HighlightCard image={snackbar} title="Web snackbar" />
+      </div>
     </PageLayout>
   );
 };
