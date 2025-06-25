2db24eb56c8792ca0ad3704553908ba6747d1f9d
Ricky Zhang <ricky.zhang@wealthsimple.com>
2025-06-24 21:26:21 -0400

    add work

--- src/pages/wealthsimple/Wealthsimple.tsx
+++ src/pages/wealthsimple/Wealthsimple.tsx
@@ -3,6 +3,8 @@ import { HighlightCard } from '../../components/HighlightCard';
 import { PageLayout } from '../../components/PageLayout';
 import darkMode from './dark-mode.webp';
 import snackbar from './snackbar.webp';
+import newProfileLayout from './new-profile-layout.webp';
+import homeGradients from './home-gradients.webp';
 
 export const Wealthsimple = () => {
   return (
@@ -12,7 +14,9 @@ export const Wealthsimple = () => {
         subtitle="Building something people love to use everyday."
       />
       <div className="grid gap-2 md:grid-cols-2">
-        <HighlightCard image={darkMode} title="Web dark mode" />
+      <HighlightCard image={homeGradients} title="Home gradients" />
+      <HighlightCard image={newProfileLayout} title="New profile layout" />
+      <HighlightCard image={darkMode} title="Web dark mode" />
         <HighlightCard image={snackbar} title="Web snackbar" />
       </div>
     </PageLayout>
