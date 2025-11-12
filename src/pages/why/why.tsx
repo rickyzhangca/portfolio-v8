efcce497bab3c4a3223ac26e63ee672c6138a040
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2025-11-11 22:44:00 -0500

    writing

--- src/pages/why/why.tsx
+++ src/pages/why/why.tsx
@@ -45,19 +45,37 @@ export const Why = () => (
       <p>
         My journey started as a UI/UX designer at Mosaic, a 3D printing startup,
         where I tackled the challenges of creating user interfaces for various
-        IoT devices.
+        IoT devices. At the same time, teams like Vercel and Linear started the
+        design engineering trend.
       </p>
       <p>
         This experience ignited my curiosity about the technical aspects,
         leading me to become a UX engineer at RBC AIOps team, a design engineer
         at Mintlify, and then Wealthsimple - my dream company making my favorite
         product. The design system team enabled me to fully leverage my
-        multi-disciplinary skills. It also pushed me further with now React
-        Native added to my stack.
+        multi-disciplinary skills. It also pushed me further with. I learned and
+        shipped features in React Native, iOS, and Android.
+      </p>
+    </div>
+    <div className="prose w-full max-w-full rounded-2xl bg-white p-3 text-start md:p-8">
+      <h2>What is design engineering to me?</h2>
+      <p>
+        Design engineering has always been changing. To me, it is about making
+        designs happen and delivering experiences in their best form.
       </p>
       <p>
-        I write about my experiences as a design engineer, sharing insights and
-        lessons learned along the way.
+        I often just tell people I am a designer, because engineering is my tool
+        to design. I code a lot because it is often a better way to design, just
+        like chefs picks the right knives based on the ingredients.
+      </p>
+      <p>
+        I jump between code and figma very often based on the needs. I code a
+        lot to untangle the technical uncertainty, get prototypes for people to
+        try, and ship things. I explore in Figma when the problem is open-ended,
+        and when creativity is needed.
+      </p>
+      <p>
+        I further wrote down where I see the future of design engineering here.
       </p>
       <a
         className="flex w-full items-center justify-center rounded-xl border border-gray-200 px-2 py-6 text-center hover:bg-gray-50"
@@ -67,6 +85,59 @@ export const Why = () => (
       >
         blog.rickyzhang.me
       </a>
+    </div>
+    <div className="prose w-full max-w-full rounded-2xl bg-white p-3 text-start md:p-8">
+      <h2>Why make things beautiful?</h2>
+      <p>
+        Because good software should be beautiful. It is the natural outcome of
+        solving problems and taking care.
+      </p>
+      <p>
+        Details and craftsmanship are a glimpse into the creator's passion and
+        dedication. It is a reflection of the creator's commitment to excellence
+        and a desire to create something meaningful.
+      </p>
+      <p>
+        The chase of delights proves the love and sacrifice went into our
+        beliefs - for the benefit of user's experience. And because that, look
+        and feel is the quickest to way to tell the quality of the software and
+        make users trust it.
+      </p>
+    </div>
+    <div className="prose w-full max-w-full rounded-2xl bg-white p-3 text-start md:p-8">
+      <h2>My values</h2>
+      <ol>
+        <li>
+          <strong>Ship things:</strong> I enjoy and celebrate the shipping
+          moments. I sometimes make fancy graphics just for myself/my team as a
+          ritual.
+        </li>
+        <li>
+          <strong>Iterative approach:</strong> I break things into chunks and
+          improve iteratively, enjoying the small wins, rather than go back and
+          forth for a long time, losing momentum.
+        </li>
+        <li>
+          <strong>Keep it simple:</strong> I am a simple person and I think from
+          other people's perspective because I want to keep things simple for
+          others too.
+        </li>
+        <li>
+          <strong>Openess:</strong> I am a deep believer of the open flow of
+          information. I share my thoughts, findings, questions, and ideas. I
+          share as much as often as possible. I post a lot in chats.
+        </li>
+        <li>
+          <strong>Remote first:</strong> Commuting and distract myself at the
+          office is meaningless attribution. The same energy should be used to
+          build beautiful things. I work full-speed in my quite room.
+        </li>
+        <li>
+          <strong>Care about work:</strong> I care about the work I do, because
+          life is short and I want to make the most of it by creating and
+          building what I believe in and will be proud of.
+        </li>
+      </ol>
     </div>
     <div className="prose w-full max-w-full rounded-2xl bg-white p-3 text-start md:p-8">
       <h2>What Drives Me</h2>
